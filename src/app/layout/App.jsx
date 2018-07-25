dc72f1ea4df222d404943ee679e507a966fcc2e4
Andy Runciman <andyrunciman@andys-mbp.home>
2018-07-25 08:21:49 +0100

    added css

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
-
+import { Container } from 'semantic-ui-react';
+import EventDashboard from '../../features/events/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar';
 class App extends Component {
   render() {
     return (
-     <h1>Re-vents</h1>
+      <React.Fragment>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
+      </React.Fragment>
     );
   }
 }
